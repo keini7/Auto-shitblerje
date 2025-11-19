41e307293a443cd10267b639a8eb2e5030388528
keini7 <hoxhakeini@gmail.com>
2025-11-19 16:28:38 +0100

    still errors...

--- carfront/App.js
+++ carfront/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "./global.css";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+
 import { AuthProvider } from "./src/context/AuthContext";
 import RootNavigation from "./src/navigation/RootNavigation";
 
 export default function App() {
   return (
     <AuthProvider>
-      <StatusBar style="light" />
-      <RootNavigation />
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <RootNavigation />
+      </NavigationContainer>
     </AuthProvider>
   );
 }
--- carfront/src/navigation/RootNavigation.jsx
+++ carfront/src/navigation/RootNavigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import BottomTabs from "./BottomTabs";
 import LoginScreen from "../screens/Account/LoginScreen";
 import RegisterScreen from "../screens/Account/RegisterScreen";
@@ -23,19 +23,15 @@ export default function RootNavigation() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isLogged ? (
-          // USER IS LOGGED → GO TO THE APP
-          <Stack.Screen name="Main" component={BottomTabs} />
-        ) : (
-          // USER NOT LOGGED → AUTH PAGES
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isLogged ? (
+        <Stack.Screen name="Main" component={BottomTabs} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 }
